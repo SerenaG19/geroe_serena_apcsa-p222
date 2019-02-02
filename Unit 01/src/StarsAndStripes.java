31a21a80ea9b910954401c6764ed7d79dd4abfd4
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-01 23:34:17 -0800

    Unit 01, final

--- Unit 01/src/StarsAndStripes.java
+++ Unit 01/src/StarsAndStripes.java
@@ -1,10 +1,10 @@
 //© A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Serena Geroe
+//Date - 02/01/19
+//Class - APCSA
+//Lab  - StarsAndStripes
 
 import static java.lang.System.*;
 
@@ -18,21 +18,34 @@ public class StarsAndStripes
 
    public void printTwentyStars()
    {
+	   out.println("********************");
    }
 
    public void printTwentyDashes()
    {
+	   out.println("--------------------");
    }
 
    public void printTwoBlankLines()
    {
+	   out.println("                    ");
+	   out.println("                    ");
    }
    
    public void printASmallBox()
    {	
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
    }
  
    public void printABigBox()
    { 	
+	   printASmallBox();
+	   printASmallBox();
    }   
 }
--- Unit 01/src/StarsRunner.java
+++ Unit 01/src/StarsRunner.java
@@ -1,10 +1,10 @@
 //© A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Serena Geroe
+//Date - 02/01/19
+//Class - APCSA
+//Lab  - StarsAndStripes
 
 import static java.lang.System.*;
 
@@ -13,8 +13,15 @@ public class StarsRunner
    public static void main(String args[])
    {
       //instantiate a StarsAndStripes object
+	   
+	   StarsAndStripes newObject = new StarsAndStripes();
       
       //call the methods needed to make the patterns on the word document
       
+	   newObject.printASmallBox();
+	   newObject.printTwoBlankLines();
+	   newObject.printABigBox();
+	   
+	   
    }
 }
