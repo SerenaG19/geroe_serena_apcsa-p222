0e231bbd0d55ede5261f29e90374f423c15811ba
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-04 16:15:10 -0800

    Slope Lab complete / Unit 02

--- Unit 02/Line.java
+++ Unit 02/Line.java
@@ -5,43 +5,45 @@
 
 public class Line
 {
-	//setting the vars
-	private int xOne, xTwo, yOne, yTwo;
-	private double slope;
-	
-	//this is the Constructor
-	public Line(int x1, int x2, int y1, int y2)
+	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		//call setCoordinates method; called the "setter"
-		setCoordinates(x1, x2, y1, y2);
+		return (double)(y1-y2)/(x1-x2);
 	}
+
 	
-	public void setCoordinates(int x1, int x2, int y1, int y2)
-	{
-		//sets "private instance variables"
-		xOne=x1;
-		xTwo=x2;
-		yOne=y1;
-		yTwo=y2;
-	}
-	
-	public void calculateSlope()
-	{
-		//uses "casting" to make calculation accurate and compatible
-		slope=(double)(yTwo-yOne)/(xTwo-xOne);
-	}
-	
-	
-	public void print()
-	{
-		//string uses two decimal pts (see slide deck)
-		System.out.printf("the slope is %.2f\n", slope);
-	}
-	
-//  given code:	
-//	public static double getSlope( int x1, int y1, int x2, int y2 )
+	//The following code is an alternative to the code above that uses
+	//object instantiation instead of using just the method
+//	//setting the vars
+//	private int xOne, xTwo, yOne, yTwo;
+//	private double slope;
+//	
+//	//this is the Constructor
+//	public Line(int x1, int x2, int y1, int y2)
 //	{
-//		return 0;
+//		//call setCoordinates method; called the "setter"
+//		setCoordinates(x1, x2, y1, y2);
+//	}
+//	
+//	public void setCoordinates(int x1, int x2, int y1, int y2)
+//	{
+//		//sets "private instance variables"
+//		xOne=x1;
+//		xTwo=x2;
+//		yOne=y1;
+//		yTwo=y2;
+//	}
+//	
+//	public void calculateSlope()
+//	{
+//		//uses "casting" to make calculation accurate and compatible
+//		slope=(double)(yTwo-yOne)/(xTwo-xOne);
+//	}
+//	
+//	
+//	public void print()
+//	{
+//		//string uses two decimal pts (see slide deck)
+//		System.out.printf("the slope is %.2f\n", slope);
 //	}
 
 }
--- Unit 02/LineRunner.java
+++ Unit 02/LineRunner.java
@@ -15,12 +15,12 @@ public class LineRunner
 //	   line.print();
 	   
 
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,9,14,2 ) );
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,18,3 ) );
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 6,4,2,2 ) );
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 4,4,5,3 ) );
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,1,2,9 ) );
-//		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,2,9 ) );
-//		
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,9,14,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,18,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 6,4,2,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 4,4,5,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,1,2,9 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,2,9 ) );
+
 	}
 }
