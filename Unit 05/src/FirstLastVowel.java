2d8051942db6328b78c92a359e25e242e7f6a86f
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-06 11:58:46 -0800

    continuing unit 03

---
+++ Unit 05/src/FirstLastVowel.java
@@ -0,0 +1,30 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+
+public class FirstLastVowel
+{
+   public static String go( String a )
+	{
+		return "add code here";
+		
+		private int x;
+		
+		public static String go( String a )
+		{
+			String vowels = "AEIOUaeiou";
+			
+			int pos1 = vowels.indexOf( a.substring(0,1)  );
+			
+			int pos2 = vowels.indexOf( a.substring(a.length()-1 )  );
+		
+			if( pos1 > -1)
+				return "yes";
+			if( pos2 > -1)
+				return "yes";
+			return "no";
+		}
+	}
+}
