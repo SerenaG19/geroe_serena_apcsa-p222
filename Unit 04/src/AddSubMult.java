5965e37274affd7fdfb2bd4c11286e24183ca6e5
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-08 10:51:27 -0800

    unit four first upload

--- Unit 04/src/AddSubMult.java
+++ Unit 04/src/AddSubMult.java
@@ -1,12 +1,29 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import static java.lang.System.*; 
 import java.util.Scanner;
 
 public class AddSubMult
 {
+	//instance variables
+	double varA, varB;
+	
+	//default constructor
+	public AddSubMult(){
+		setVars(0,0);
+	}
+	
+	//initializing constructor
+	public AddSubMult(double a, double b){
+		setVars(varA,varB;)
+	}
+	
+	public void setVars(double a, double b) {
+		
+	}
+	
 	public static double check( double a, double b )
 	{
 		return 0;
