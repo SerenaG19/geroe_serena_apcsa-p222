fee69eb7f10a6f2ac54d1abe69de320a95035d6c
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-12 17:49:31 -0800

    Unit 06: Count Pairs Lab complete

--- Unit 06/src/CountPairs.java
+++ Unit 06/src/CountPairs.java
@@ -1,14 +1,31 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name - Serena Geroe
 
 import static java.lang.System.*;
 
 public class CountPairs
 {
 	public static int pairCounter( String str )
-	{ 
-		int count = 0;
-		return count;
+	{ 	
+		int pairNum = 0;
+
+			for(int count = 1; count <= str.length()-1; count++)
+			{
+				
+				if( str.substring(count-1,count).equals( str.substring(count,count+1) ) )
+				{
+					pairNum++;
+
+				}
+
+			}
+			return pairNum;
+		
 	}
 }
+
+//for(char lookPairs : str.toCharArray() )
+//{			
+//	out.println(lookPairs);
+//}
