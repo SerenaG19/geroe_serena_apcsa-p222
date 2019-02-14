57e099a73c3881b3881f1009464705678e71710b
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-13 19:43:17 -0800

    unit 06 / progress on Letter remover and perfect

--- Unit 06/src/LetterRemover.java
+++ Unit 06/src/LetterRemover.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import static java.lang.System.*;
 
@@ -31,14 +31,20 @@ public class LetterRemover
 	public String removeLetters()
 	{
 		char sentArray[] = sentence.toCharArray();
-		for(int i = 0; i<sentArray.length; i++)
+		for(int i = 0; i<sentArray.length-2; i++)
 		{
-			if(sentArray[i] == lookFor)
+			char tryNext = sentArray[i+1];
+			for(int counter = 0; counter < sentArray.length-2; counter++)
 			{
-				sentArray[i] = sentArray[i+1];
+				if(sentArray[i] == lookFor && sentArray[i] != sentArray[i+1])
+				{
+					sentArray[i] = tryNext;
+				}
+				else
+				{
+					tryNext = sentArray[i++];
+				}
 			}
-			System.out.print(sentArray[i]);
-			//System.out.print("i = " + i);
 		}
 	
 		String cleaned = new String(sentArray);
@@ -47,6 +53,28 @@ public class LetterRemover
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor + " = ";
+		return sentence + " - letter to remove " + lookFor;
 	}
 }
+
+//if(sentArray[i] == lookFor)
+//{
+//	sentArray[i] = sentArray[i+1];
+//}
+//System.out.print(sentArray[i]);
+////System.out.print("i = " + i);
+
+
+
+
+//if(sentArray[i] == lookFor && sentArray[i+1] != lookFor)
+//{
+//	sentArray[i] = sentArray[i+1];
+//}
+//else
+//{
+//	for(int counter = 0; counter < sentArray.length; counter++)
+//	{
+//		
+//	}
+//}
--- Unit 06/src/Perfect.java
+++ Unit 06/src/Perfect.java
@@ -1,19 +1,33 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import static java.lang.System.*;
 
 public class Perfect
 {
    private int number;
-
-	//add constructors
+   
+ //add constructors   
+   public Perfect()
+   {
+	   number = 0;
+   }
+   
+   public Perfect(int num)
+   {
+	   setNum(num);
+   }
 
 	//add a set method
-
+   public void setNum(int num)
+   {
+	   number = num;
+   }
+   
 	public boolean isPerfect()
 	{
+		if()
 		return false;
 	}
 
