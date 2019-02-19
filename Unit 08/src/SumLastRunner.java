5a2c0fcf52275163994ac0ce973e7ed680909f69
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-19 10:24:31 -0800

    First draft, RaySumLast lab, unit 08

--- Unit 08/src/SumLastRunner.java
+++ Unit 08/src/SumLastRunner.java
@@ -1,11 +1,12 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Serena Geroe
+//Date - 
 
 public class SumLastRunner
 {
 	public static void main( String args[] )
-	{											
+	{
+		System.out.println( s.go (new int[] {-99,1,2,3,4,5,6,7,8,9,10,5} ) );
 	}
 }
