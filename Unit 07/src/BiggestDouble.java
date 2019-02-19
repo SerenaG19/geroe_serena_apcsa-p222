c13efa2b0201732040f8e386c0187222de4cb221
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-19 11:16:02 -0800

    first draft of greatest double / Unit 7

--- Unit 07/src/BiggestDouble.java
+++ Unit 07/src/BiggestDouble.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 // www.apluscompsci.com
-//Name -  
+//Name - Serena Geroe
 
 import static java.lang.System.*;
 
@@ -10,24 +10,33 @@ public class BiggestDouble
 
 	public BiggestDouble()
 	{
-		this(0,0,0,0);
+		setDoubles(0,0,0,0);
 	}
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		if(one > two && one > three && one > four) return one;
+		if(two > one && two > three && two > four) return two;
+		if(three > one && three > two && three > four) return two;
+		if(four > one && four > two && four > three) return four;
+		else return 0;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return one + " " + two + " " + three + " " + four + " \n" + "biggest = " + getBiggest();
 	}
 }
