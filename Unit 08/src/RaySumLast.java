5cf6695aadddc9ceb4d299dc3e0e07943b73dc0a
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-24 10:55:35 -0800

    progress on unit 08 / raysumlast

--- Unit 08/src/RaySumLast.java
+++ Unit 08/src/RaySumLast.java
@@ -1,26 +1,53 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name - Serena Geroe
-//Date -
+//Date - 02/24/19
 
 public class RaySumLast
-{
+{	
+	private int array[];
+	
+	public RaySumLast()
+	{
+		
+	}
+	
+	public RaySumLast(int[] ray)
+	{
+		
+	}
+	
+	public void setRay(int[] ray)
+	{
+		for(int i = 0; i < ray.length; i++)
+		{
+			array[i] = ray[i];
+		}
+	}
+
 	public static int go(int[] ray)
 	{
 		if(ray.length == 0 )
 		{
 			return -1;
 		}
-		int val = ray[ ray.length-1];
+		
+		int last = ray[ray.length-1];
 		int sum = 0;
 		
 		for(int i=0; i < ray.length-1; i++)
 		{
-			if( ray[i] > val )
+			if( ray[i] > last )
 				sum = sum+ray[i];
 		}
 		if(sum == 0)
 			return -1;
 		return sum;
+	}
+	
+	public String toString()
+	{
+		String output = "";
+		return output;
 	}
 }
--- Unit 08/src/SumLastRunner.java
+++ Unit 08/src/SumLastRunner.java
@@ -1,12 +1,13 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name - Serena Geroe
-//Date - 
+//Date - 02/24/19
 
 public class SumLastRunner
 {
 	public static void main( String args[] )
 	{
-		System.out.println( s.go (new int[] {-99,1,2,3,4,5,6,7,8,9,10,5} ) );
+		RaySumLast test = new RaySumLast();
+		System.out.println( test.go(new int[] {-99,1,2,3,4,5,6,7,8,9,10,5}) );
 	}
 }
