f0b357ab2d2637447eb8a5dd4c96ccf0f5e03d8f
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-02-25 13:21:47 -0800

    draft 1 for RayDown lab / unit 08
    Arrays not initialized properly...

--- Unit 08/src/RayDown.java
+++ Unit 08/src/RayDown.java
@@ -1,14 +1,33 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Serena Geroe
+//Date - 02/25/19
+
+/*Prompt:
+ * Write a program that will go through an array to see if the numbers are decreasing.
+ * You must combine variables, ifs, and a loop to create a working method. There will always be at least one
+ * item in the array. Each prior number has to be greater than the following number for the array to be
+ * considered going down. 
+ */
 
 public class RayDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
+	
+
+	
 	public static boolean go(int[] numArray)
-	{
-		return true;
+	{	
+		int len = numArray.length;
+		boolean dec = false;
+		numArray = new int[len];
+		for(int i = 0; i<numArray.length-1; i++)
+		{
+			if(numArray[i] > numArray[i+1]) dec = true;
+			System.out.println(numArray[i] + "i: " + i + " . dec : " + dec);
+		}
+		
+		return dec;
 	}	
 }
