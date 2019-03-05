dbc92e794a00b614938481b6d1528562aca27d6b
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-04 16:36:38 -0800

    unit 09 labs - listsumfirst first attempt

--- Unit 09 Labs/src/ListSumFirst.java
+++ Unit 09 Labs/src/ListSumFirst.java
@@ -1,15 +1,25 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Serena Geroe
+//Date - 03/04/19
 
 import java.util.List;
 import java.util.ArrayList;
 
+
 public class ListSumFirst
 {
-	public static int go(List<Integer> ray)
+	public static int go(List<Integer>ray)
 	{
-		return 0;
+		ray = new ArrayList<Integer>();
+		if(ray.size() == 0) return -1;
+		
+		int count = 0;
+		for(int i = 0; i < ray.size(); i++)
+			if(ray.get(i) > ray.get(0)) count += ray.get(i);
+
+		if(count == 0) return -1;
+		
+		return count;
 	}
 }
--- Unit 09 Labs/src/SumFirstRunner.java
+++ Unit 09 Labs/src/SumFirstRunner.java
@@ -1,11 +1,20 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Serena Geroe
+//Date - 03/04/19
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SumFirstRunner
 {
 	public static void main( String args[] )
-	{										
+	{						
+		ArrayList<Integer>test;
+		test = new ArrayList<Integer>();
+		test.add(32767);
+		
+		System.out.println( ListSumFirst.go(test) );
 	}
 }
