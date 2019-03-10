c5893fa25238fa3d37ad9f50408c77da2c5ec0eb
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-10 11:26:54 -0700

    progress on Toys Part II

--- Unit 11/src/ToyStoreRunner.java
+++ Unit 11/src/ToyStoreRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -11,6 +11,20 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter toy names :: ");
+		String response = keyboard.nextLine();
+		Scanner chop = new Scanner(response);
+		
+		String currentNm = "";
+		while(chop.hasNext())
+		{
+			currentNm = chop.next();
+//			ToyStore.loadToys(currentNm);
+		}
 
+		
+	
+	
 	}
 }
