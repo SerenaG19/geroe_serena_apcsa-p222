cf2dfb1a6e666f94ae163696160d296db5e8fafb
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-13 21:20:58 -0700

    progess on rational lab / unit 11

--- Unit 11/src/Rational.java
+++ Unit 11/src/Rational.java
@@ -12,64 +12,90 @@ class Rational implements Comparable<Rational>
 	//write two constructors
 	public Rational()
 	{
-		
+		setRational(1,1);
+	}
+	
+	public Rational(int top, int bot)
+	{
+		setRational(top,bot);
+	}
+	
+	//write a setRational method
+	public void setRational(int top, int bot)
+	{
+		num = top;
+		den = bot;
 	}
 
-
-	//write a setRational method
-	
-
 	//write  a set method for numerator and denominator
-
+	public void setNum(int top)
+	{
+		num = top;
+	}
+	
+	public void setDen(int bot)
+	{
+		den = bot;
+	}
 	
 	public void add(Rational  other)
 	{
 		//num1/den1 + num2/den2 
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
-		
-		
-
-
+		num = (num * other.getDen()) + (other.getNum() * den);
+		den = den * other.getDen();
 		reduce();
 	}
 
 	private void reduce()
 	{
-
-
-
+		num /= gcd(num,den);
+		den /= gcd(num,den);
 	}
 
 	private int gcd(int numOne, int numTwo)
 	{
+		int smaller;
+		if(numOne <= numTwo) smaller = numOne;
+		else smaller = numTwo;
+		
+		int factor = 0;
+		for(int i = 1; i < (smaller+1)/2; i++)
+		{
+			if(smaller % i == 0 && i > factor) factor = i;
+		}
 
-
-		return 1;
+		return factor;
 	}
 
 	public Object clone ()
 	{
-		return "";
+		return num + "/" + den;
 	}
 
-
 	//ACCESSORS
-
 	//write get methods for numerator and denominator
-	
-	
-	public boolean equals( Object obj)
+	public int getNum()
 	{
-
-
+		return num;
+	}
+	
+	public int getDen()
+	{
+		return den;
+	}
+	
+	
+	public boolean equals( Object obj )
+	{
+		return obj.equals(Rational);
 		return false;
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
+		if()
 		return -1;
 	}
 
@@ -77,6 +103,10 @@ class Rational implements Comparable<Rational>
 
 	
 	//write  toString() method
+//	public String toString()
+//	{
+//		
+//	}
 	
 	
 }
