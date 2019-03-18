44b5d9d7a9599f0f11c2449f02423223e8564c0a
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-18 10:18:48 -0700

    initial commit for elevens act 9

---
+++ Unit 09/src/activity9/Deck.java
@@ -0,0 +1,161 @@
+package activity9;
+import java.util.List;
+
+import activity1.Card;
+
+import java.util.ArrayList;
+
+/**
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ *      initialize, shuffle, deal, and check if empty.
+ */
+public class Deck {
+
+	/**
+	 * cards contains all the cards in the deck.
+	 */
+	//private List<Card> cards; // Don't use this yet!
+
+	/**
+	 * size is the number of not-yet-dealt cards.
+	 * Cards are dealt from the top (highest index) down.
+	 * The next card to be dealt is at size - 1.
+	 */
+	private int size;
+	
+
+	private Card[] cards;
+	/**
+	 * Creates a new <code>Deck</code> instance.<BR>
+	 * It pairs each element of ranks with each element of suits,
+	 * and produces one of the corresponding card.
+	 * @param ranks is an array containing all of the card ranks.
+	 * @param suits is an array containing all of the card suits.
+	 * @param values is an array containing all of the card point values.
+	 */
+	public Deck(String[] ranks, String[] suits, int[] values) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		
+		//MAKE SURE YOU UNDERSTAND THIS; fair game for assessments, and lots of assignments
+		//be able to do this quickly
+		
+		//instantiate new array of cards
+		//loop over ranks then suits (nested loop)
+		//add new card object to the array
+		//later on, call shuffle method in Act 4
+		
+		cards = new Card[suits.length*ranks.length];
+		
+		for(int i = 0; i < cards.length; i=i)
+		{
+			for(int j = 0; j < ranks.length; j++) 
+			{
+				for (int k = 0; k < suits.length; k++)
+				{
+					cards[i] = new Card(ranks[j], suits[k], values[j]);
+					size++;
+					i++;
+				}
+			}
+		}
+		shuffle();		
+	}
+
+
+	/**
+	 * Determines if this deck is empty (no undealt cards).
+	 * @return true if this deck is empty, false otherwise.
+	 */
+	public boolean isEmpty() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		//check size to see if deck is empty
+		return size == 0;	
+	}
+
+	/**
+	 * Accesses the number of undealt cards in this deck.
+	 * @return the number of undealt cards in this deck.
+	 */
+	public int size() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size;
+	}
+
+	/**
+	 * Randomly permute the given collection of cards
+	 * and reset the size to represent the entire deck.
+	 */
+	public void shuffle() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+		
+		//this should work for just arrays for now, will be asked later to make it work with array lists
+		//use the efficient selectionShuffle
+		
+		Card[] shuffled = new Card[cards.length];
+		int r;
+		
+		for(int i = cards.length-1; i >= 0; i--)
+		{
+			r = (int) ( Math.random() * (i+1) );
+			shuffled[i] = cards[r];
+			cards[r] = cards[i];
+		}
+		
+		for(int q = 0; q < cards.length; q++) cards[q] = shuffled[q];
+	}
+
+	/**
+	 * Deals a card from this deck.
+	 * @return the card just dealt, or null if all the cards have been
+	 *         previously dealt.
+	 */
+	public Card deal() {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		//check if deck is empty
+		//decrement deck size (pull a card off top one at a time)
+		//return card object from array
+		if(isEmpty()) return null;
+		size--;
+		return cards[size];		
+	}
+
+	/**
+	 * Generates and returns a string representation of this deck.
+	 * @return a string representation of this deck.
+	 */
+	@Override
+	public String toString() {
+		String rtn = "size = " + size + "\nUndealt cards: \n";
+
+		for (int k = size - 1; k >= 0; k--) {
+			//rtn = rtn + cards.get(k);
+			rtn = rtn + cards[k];
+			if (k != 0) {
+				rtn = rtn + ", ";
+			}
+			if ((size - k) % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\nDealt cards: \n";
+//		for (int k = cards.size() - 1; k >= size; k--) {
+		for (int k = cards.length - 1; k >= size; k--) {
+//			rtn = rtn + cards.get(k);
+			rtn = rtn + cards[k];
+			if (k != size) {
+				rtn = rtn + ", ";
+			}
+//			if ((k - cards.size()) % 2 == 0) {
+			if (k - cards.length % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\n";
+		return rtn;
+	}
+}
--- Unit 12/src/Word.java
+++ Unit 12/src/Word.java
@@ -13,41 +13,51 @@ public class Word implements Comparable<Word>
 		word = s;
 	}
 	
-	public int getLength()
-	{
-		return word.length();
-	}
+//	public int getLength()
+//	{
+//		return word.length();
+//	}
 	
 	public int compareTo( Word rhs )
 	{	
-		char[] alpha = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		
-//		int indWord = 0;
-//		int indOther = 0;
 		
-		char[] myWord = new char[word.length()];
-		for(int i = 0; i < word.length(); i++)
-		{
-			myWord[i] = word.charAt(i);
-		}
 		
-		char[] other = new char[rhs.getLength()];
-		for(int i = 0; i < other.length; i++)
-		{
-			other[i] = rhs.charAt(i);
-		}
 		
-		if(word.length() > rhs.getLength()) return 1;
-		else if(word.length() < rhs.getLength()) return -1;
-		else
-		{
-			
-//			for(int i = 0; i < alpha.length; i++)
-//			{
-//				if(word.charAt(0) == alpha[i]) indWord = i;
-//				else if(((CharSequence) rhs).charAt(0) == alpha[i]) indOther = i;
-//			}
-		}
+		
+		
+		
+		
+		
+		
+		
+		//		char[] alpha = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+//		
+////		int indWord = 0;
+////		int indOther = 0;
+//		
+//		char[] myWord = new char[word.length()];
+//		for(int i = 0; i < word.length(); i++)
+//		{
+//			myWord[i] = word.charAt(i);
+//		}
+//		char[] otherRay = new char[rhs.getLength()];
+//		for(int i = 0; i < otherRay.length; i++)
+//		{
+//			String other = (String) rhs;
+//			other[i] = rhs.charAt(i);
+//		}
+//		if(word.length() > rhs.getLength()) return 1;
+//		else if(word.length() < rhs.getLength()) return -1;
+//		else
+//		{
+//			
+////			for(int i = 0; i < alpha.length; i++)
+////			{
+////				if(word.charAt(0) == alpha[i]) indWord = i;
+////				else if(((CharSequence) rhs).charAt(0) == alpha[i]) indOther = i;
+////			}
+//		}
 		
 		return 1;
 //		return -1;
