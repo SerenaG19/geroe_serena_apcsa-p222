27468e14c1a68ecf253224144e0238f9101913fc
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-18 11:13:41 -0700

    update word unit 12

--- Unit 12/src/Word.java
+++ Unit 12/src/Word.java
@@ -12,56 +12,17 @@ public class Word implements Comparable<Word>
 	{
 		word = s;
 	}
-	
-//	public int getLength()
-//	{
-//		return word.length();
-//	}
-	
+		
 	public int compareTo( Word rhs )
 	{	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//		char[] alpha = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-//		
-////		int indWord = 0;
-////		int indOther = 0;
-//		
-//		char[] myWord = new char[word.length()];
-//		for(int i = 0; i < word.length(); i++)
-//		{
-//			myWord[i] = word.charAt(i);
-//		}
-//		char[] otherRay = new char[rhs.getLength()];
-//		for(int i = 0; i < otherRay.length; i++)
-//		{
-//			String other = (String) rhs;
-//			other[i] = rhs.charAt(i);
-//		}
-//		if(word.length() > rhs.getLength()) return 1;
-//		else if(word.length() < rhs.getLength()) return -1;
-//		else
-//		{
-//			
-////			for(int i = 0; i < alpha.length; i++)
-////			{
-////				if(word.charAt(0) == alpha[i]) indWord = i;
-////				else if(((CharSequence) rhs).charAt(0) == alpha[i]) indOther = i;
-////			}
-//		}
-		
-		return 1;
-//		return -1;
-//		return 0;
+		if(word.length() < rhs.toString().length()) return -1; //because want ascending order
+		else if(word.length() > rhs.toString().length()) return 1;
+		else
+		{
+			if(word.compareTo(rhs.toString() ) < 0) return -1; // because word is before rhs here
+			else if(word.compareTo(rhs.toString() ) < 0) return 1;
+			else return 0;
+		}
 	}
 
 	public String toString()
@@ -69,3 +30,45 @@ public class Word implements Comparable<Word>
 		return word;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+//		char[] alpha = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+//
+////int indWord = 0;
+////int indOther = 0;
+//
+//char[] myWord = new char[word.length()];
+//for(int i = 0; i < word.length(); i++)
+//{
+//	myWord[i] = word.charAt(i);
+//}
+//char[] otherRay = new char[rhs.getLength()];
+//for(int i = 0; i < otherRay.length; i++)
+//{
+//	String other = (String) rhs;
+//	other[i] = rhs.charAt(i);
+//}
+//if(word.length() > rhs.getLength()) return 1;
+//else if(word.length() < rhs.getLength()) return -1;
+//else
+//{
+//	
+////	for(int i = 0; i < alpha.length; i++)
+////	{
+////		if(word.charAt(0) == alpha[i]) indWord = i;
+////		else if(((CharSequence) rhs).charAt(0) == alpha[i]) indOther = i;
+////	}
+//}
+
+//return 1;
+//return -1;
+//return 0;
--- Unit 12/src/WordRunner.java
+++ Unit 12/src/WordRunner.java
@@ -5,38 +5,79 @@
 import java.io.File; 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+
 import static java.lang.System.*;
 
 public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("words.dat"));
+		Scanner file = new Scanner(new File("H:\\APCSA Units\\geroe_serena_apcsa-p222\\Unit 12\\src\\words.dat")); 
+		// make sure to have two slashes
 
 		int size = file.nextInt();
 		file.nextLine();
 		
-		Comparable[] myWords = new Comparable[size];
-		for(int i = 0; i < size; i++)
+		ArrayList<String>myWords = new ArrayList();
+		
+		
+//		for(int i = 0; i < size-1; i++)
 		{
-			myWords[i] = file.next();
-			file.nextLine();
+			for(int j = 0; j < size; j++)
+			{
+				String first = new String(file.next());
+				file.nextLine();
+				String second = new String(file.next());
+				file.nextLine();
+				
+				if(first.compareTo(second) == 1 || first.compareTo(second) == 0)
+				{
+					myWords.add(j,first);
+					myWords.add(j+1,second);
+				}
+				else
+				{
+					myWords.add(j,second);
+					myWords.add(j+1,first);
+				}
+			}
 		}
 		
-		Arrays.sort(myWords);
-		System.out.println(Arrays.toString(myWords));
-		
-
-
-
-
-
-
-
-
-
-
+		for(int n = 0; n < myWords.size(); n++)
+		{
+			System.out.println(myWords.get(n));
+		}
 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+////Comparable[] myWords = new Comparable[size];
+//
+//ArrayList<Comparable>myWords = new ArrayList<Comparable>();
+//
+//for(int i = 0; i < size; i++)
+//{
+////	myWords[i] = file.next();
+//	myWords.add(file.next());
+//	file.nextLine();
+//}
+//
+////Arrays.sort(myWords);
+////System.out.println(Arrays.toString(myWords));
+//
+//
+//Collections.sort(myWords);
+//System.out.println(myWords);
