31738562e09ba29128311e099ea29a22f0e2ba0c
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-20 09:50:28 -0700

    word lab complete - unit 12 finished

--- Unit 12/src/Word.java
+++ Unit 12/src/Word.java
@@ -19,8 +19,8 @@ public class Word implements Comparable<Word>
 		else if(word.length() > rhs.toString().length()) return 1;
 		else
 		{
-			if(word.compareTo(rhs.toString() ) < 0) return -1; // because word is before rhs here
-			else if(word.compareTo(rhs.toString() ) < 0) return 1;
+			if(word.toString().compareTo(rhs.toString() ) < 0) return -1; // because word is before rhs here
+			else if(word.toString().compareTo(rhs.toString() ) > 0) return 1;
 			else return 0;
 		}
 	}
@@ -30,45 +30,3 @@ public class Word implements Comparable<Word>
 		return word;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-//		char[] alpha = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-//
-////int indWord = 0;
-////int indOther = 0;
-//
-//char[] myWord = new char[word.length()];
-//for(int i = 0; i < word.length(); i++)
-//{
-//	myWord[i] = word.charAt(i);
-//}
-//char[] otherRay = new char[rhs.getLength()];
-//for(int i = 0; i < otherRay.length; i++)
-//{
-//	String other = (String) rhs;
-//	other[i] = rhs.charAt(i);
-//}
-//if(word.length() > rhs.getLength()) return 1;
-//else if(word.length() < rhs.getLength()) return -1;
-//else
-//{
-//	
-////	for(int i = 0; i < alpha.length; i++)
-////	{
-////		if(word.charAt(0) == alpha[i]) indWord = i;
-////		else if(((CharSequence) rhs).charAt(0) == alpha[i]) indOther = i;
-////	}
-//}
-
-//return 1;
-//return -1;
-//return 0;
--- Unit 12/src/WordRunner.java
+++ Unit 12/src/WordRunner.java
@@ -15,56 +15,37 @@ public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-//		Scanner file = new Scanner(new File("H:\\APCSA Units\\geroe_serena_apcsa-p222\\Unit 12\\src\\words.dat")); //COMPUTER AT SCHOOL
-		Scanner file = new Scanner(new File("C:\\Users\\Serena\\Desktop\\geroe_serena_apcsa-p222\\Unit 12\\src\\words.dat")); //COMPUTER AT HOME
+		Scanner file = new Scanner(new File("H:\\APCSA Units\\geroe_serena_apcsa-p222\\Unit 12\\src\\words.dat")); //COMPUTER AT SCHOOL
+//		Scanner file = new Scanner(new File("C:\\Users\\Serena\\Desktop\\geroe_serena_apcsa-p222\\Unit 12\\src\\words.dat")); //COMPUTER AT HOME
 		// make sure to have two slashes
 
 		int size = file.nextInt();
 		file.nextLine();
 		
-		ArrayList<String>myWords = new ArrayList<String>();
+		ArrayList<Word>myWords = new ArrayList<Word>();
 		
 		//load words into list
 		for(int i = 0; i < size; i++)
 		{
-			String load = file.next();
+			Word load = new Word(file.next());
 			myWords.add(load);
 //			System.out.println(i + ":: " +load);
 		}
 				
 		//put list into order
-//		for(int k = 0; k < size - 1; k++)
-//		{
-//			for(int i = 0; i < myWords.size() - 1; i++)
-//			{
-//				System.out.println("i :: "+ i);
-//				
-//				System.out.println("myWords.get(i):: "+myWords.get(i)+" vs myWords.get(i+1):: "+myWords.get(i+1));
-//				if(myWords.get(i).compareTo(myWords.get(i+1)) > 0)
-//				{
-//					String temp = myWords.get(i);
-//					myWords.set(i, myWords.get(i+1));
-//					myWords.set(i+1, temp);
-//					System.out.println("myWords.get(i).length() > myWords.get(i+1).length() since "+myWords.get(i).length()+" > "+myWords.get(i+1).length());
-//				}
-//				else i++;
-//				
-//				System.out.println("Current myWords list :: " + myWords + "\n");
-//
-//			}
-//		}
-		
-		for(int i = 0; i < size - 1; i++)
+
+		for(int h = 0; h < size; h++)
 		{
-			if(myWords.get(i).compareTo(myWords.get(i+1)) > 0)
+			for(int i = 0; i < size - 1; i++)
 			{
-				String temp = myWords.get(i);
-				myWords.set(i, myWords.get(i+1));
-				myWords.set(i+1, temp);
-				System.out.println("myWords.get(i).length() > myWords.get(i+1).length() since "+myWords.get(i).length()+" > "+myWords.get(i+1).length());
+				if(myWords.get(i).compareTo(myWords.get(i+1)) > 0)
+				{
+					Word temp = myWords.get(i);
+					myWords.set(i, myWords.get(i+1));
+					myWords.set(i+1, temp);
+				}
 			}
 		}
-		
 
 		//print
 		for(int n = 0; n < myWords.size(); n++)
@@ -74,50 +55,3 @@ public class WordRunner
 
 	}
 }
-
-
-//for(int i = 0, j = 0; i < size-1; i++)
-//{
-//	for(int j = 0; j < size-1; j++)
-//	{
-//		
-//		System.out.println("\ni :: "+i+";\tj :: "+j);
-//		
-//		
-//		if(myWords.get(j).length() < myWords.get(j+1).length())
-//		{
-//			j++;
-//			System.out.println("myWords.get(j).length() < myWords.get(j+1).length() since "+myWords.get(j).length()+" < "+myWords.get(j+1).length());
-//		}
-//		
-//		else if(myWords.get(j).length() > myWords.get(j+1).length())
-//		{					
-//			String temp = myWords.get(j);
-//			myWords.set(j, myWords.get(j+1));
-//			myWords.set(j+1, temp);
-//			System.out.println("myWords.get(i).length() > myWords.get(i+1).length() since "+myWords.get(j).length()+" > "+myWords.get(j+1).length());
-//
-//		}
-//		else
-//		{System.out.println("myWords.get(j).length() = myWords.get(j+1).length() since "+myWords.get(j).length()+" = "+myWords.get(j+1).length());
-//
-//			if(myWords.get(j).compareTo(myWords.get(j+1)) > 0)
-//			{
-//				String temp = myWords.get(j);
-//				myWords.set(j, myWords.get(j+1));
-//				myWords.set(j+1, temp);
-//				System.out.println("Must swap: myWords.get(j).compareTo(myWords.get(j+1)) > 0");
-//			}
-//			else 
-//			{
-//				j++;
-//				System.out.println("In order: myWords.get(i).compareTo(myWords.get(j+1)) <= 0");
-//			}
-//		}
-//
-//		System.out.println("myWords.get(j) :: " + myWords.get(j) + "\t myWords.get(j+1) :: " + myWords.get(j+1));
-//		System.out.println("myWords.get(j).compareTo(myWords.get(j+1)) :: "+myWords.get(j).compareTo(myWords.get(j+1)));				
-//		System.out.println("Current myWords list :: " + myWords + "\n");
-//	}
-//	
-//}
