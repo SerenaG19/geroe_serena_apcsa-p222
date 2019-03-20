bc432559ec463af37e7f2700f4f44793d1d968ca
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-20 11:03:12 -0700

    Unit 12 complete (final version of words lab)

--- Unit 12/src/WordRunner.java
+++ Unit 12/src/WordRunner.java
@@ -22,46 +22,48 @@ public class WordRunner
 		int size = file.nextInt();
 		file.nextLine();
 		
-//		ArrayList<Word>myWords = new ArrayList<Word>();
 		Word[] myWords = new Word[size];
 		
-		//load words into list
 		for(int i = 0; i < size; i++)
 		{
 			Word load = new Word(file.next());
 			myWords[i] = load;
-//			myWords.add(load);
-//			System.out.println(i + ":: " +load);
 		}
-				
-		//put list into order
+		
 		Arrays.sort(myWords);
 		
-		//print
-		System.out.println(Arrays.toString(myWords));
+		for(int n = 0; n < myWords.length; n++)
+		{
+			System.out.println(myWords[n]);
+		}
 		
-//		for(int h = 0; h < size; h++)
-//		{
-//			for(int i = 0; i < size - 1; i++)
-//			{
-//				if(myWords[i].compareTo(myWords[i+1] > 0))
-////				if(myWords.get(i).compareTo(myWords.get(i+1)) > 0)
-//				{
-//					Word temp = myWords[i];
-//					myWords[i] = myWords[i+1];
-//					myWords[i+1] = temp;
-////					Word temp = myWords.get(i);
-////					myWords.set(i, myWords.get(i+1));
-////					myWords.set(i+1, temp);
-//				}
-//			}
-//		}
-
-		//print
-//		for(int n = 0; n < myWords.size(); n++)
-//		{
-//			System.out.println(myWords.get(n));
-//		}
-
+//		System.out.println(Arrays.toString(myWords));
 	}
 }
+
+
+//ArrayList<Word>myWords = new ArrayList<Word>();
+//load words into list
+//	myWords.add(load);
+//	System.out.println(i + ":: " +load);
+//for(int h = 0; h < size; h++)
+//{
+//	for(int i = 0; i < size - 1; i++)
+//	{
+//		if(myWords[i].compareTo(myWords[i+1] > 0))
+////		if(myWords.get(i).compareTo(myWords.get(i+1)) > 0)
+//		{
+//			Word temp = myWords[i];
+//			myWords[i] = myWords[i+1];
+//			myWords[i+1] = temp;
+////			Word temp = myWords.get(i);
+////			myWords.set(i, myWords.get(i+1));
+////			myWords.set(i+1, temp);
+//		}
+//	}
+//}
+//print
+//for(int n = 0; n < myWords.size(); n++)
+//{
+//	System.out.println(myWords.get(n));
+//}
