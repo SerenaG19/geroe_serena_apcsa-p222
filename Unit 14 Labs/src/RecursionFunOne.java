64be35f14651b1de3a6a1d86e96a3c1fafc8a946
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-21 18:01:46 -0700

    count even digits lab complete / unit 14

--- Unit 14 Labs/src/RecursionFunOne.java
+++ Unit 14 Labs/src/RecursionFunOne.java
@@ -1,15 +1,33 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import static java.lang.System.*;
 
 public class RecursionFunOne
-{
+
+/*if ( num is greater than zero )
+ * 	if the digit is even
+ * 		return 1 plus a call with remaining digits
+ * 	return 0 plus a call with remaining digits
+ */
+{			//should really be countEVENDigits, but functions as such
 	public static int countOddDigits(int num)
 	{
-
-
-		return 0;
+//		System.out.println("num :: "+num);
+		if(num > 0)
+		{
+//			System.out.println("num > 0");
+			if(num % 2 == 0)
+			{
+//				System.out.println("\nnum % 2 == 0; next :: ");
+//				System.out.print(num/10);
+				return 1 + countOddDigits(num/10);
+			}
+//			System.out.println("\nnum % 2 == 1; next :: ");
+//			System.out.print(num/10);
+			return 0 + countOddDigits(num/10);
+		}
+		else return 0;
 	}
 }
