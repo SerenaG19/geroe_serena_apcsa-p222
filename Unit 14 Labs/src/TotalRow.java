0be9a950b2160e140ddadd61601f7df2ff501431
SerenaG19 <47190047+SerenaG19@users.noreply.github.com>
2019-03-21 18:35:46 -0700

    unit 14 / total row lab complete

--- Unit 14 Labs/src/TotalRow.java
+++ Unit 14 Labs/src/TotalRow.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 import java.util.*;
 
@@ -9,6 +9,26 @@ public class TotalRow
     public static List<Integer> getRowTotals( int[][] m )
     {
     	//add code here
-		return null;
+    	List<Integer> rowSums = new ArrayList<Integer>();
+    	int sum = 0;
+    	for(int i = 0; i < m.length; i++)
+    	{
+    		sum = 0;
+    		
+//    		System.out.println("\ni :: " +i);
+    		
+    		for(int j = 0; j < m[i].length; j++)
+    		{
+//    			System.out.println("sum before :: "+sum);
+    			
+    			sum+=m[i][j];
+    			
+//    			System.out.println("sum after :: "+sum);
+    		}
+    		rowSums.add(i,sum);
+//			System.out.println("rowSums list :: "+rowSums);
+
+    	}
+		return rowSums;
     }
 }
--- Unit 14 Labs/src/TotalRowRunner.java
+++ Unit 14 Labs/src/TotalRowRunner.java
@@ -1,14 +1,24 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Serena Geroe
 
 public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
 		//add code here			
+		int[][] test1 = new int[][] {{1,2,3},{5,5,5,5}};
+		System.out.println( "Row total are :: "+ TotalRow.getRowTotals(test1));
+		
+		int[][] test2 = new int[][] {{1,2,3},{5},{1},{2,2}};
+		System.out.println( "Row total are :: "+ TotalRow.getRowTotals(test2));
+		
+		int[][] test3 = new int[][] {{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
+		System.out.println( "Row total are :: "+ TotalRow.getRowTotals(test3));
 	}
 }
 
-
-
+//
+//{{1,2,3},{5,5,5,5}}
+//{{1,2,3},{5},{1},{2,2}};
+//{{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
